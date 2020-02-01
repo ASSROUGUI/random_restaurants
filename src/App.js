0c48f767073dee6580f456b77894791a4c14ab7d
ASSROUGUI <38358417+ASSROUGUI@users.noreply.github.com>
2020-02-01 18:57:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import DataList from './components/DB'
+
 import './App.css';
 import Search from './components/Search'; 
 import HomePage from './components/HomePage';
