b232b1d5ff7d238c7a7146e8b30baea8cf78ff62
ASSROUGUI <ashwagooo@hotmail.com>
2020-02-01 19:45:11 +0300

    Updates

---
+++ precache-manifest.160a20f674711a6064c37b78082a4f9b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f8150edd84c74d54c6169564860068",
+    "url": "/random_restaurants/index.html"
+  },
+  {
+    "revision": "074e6e5289009c1b2d1d",
+    "url": "/random_restaurants/static/css/2.da788843.chunk.css"
+  },
+  {
+    "revision": "a8805cfad90a80658198",
+    "url": "/random_restaurants/static/css/main.9fec64ef.chunk.css"
+  },
+  {
+    "revision": "074e6e5289009c1b2d1d",
+    "url": "/random_restaurants/static/js/2.79c83804.chunk.js"
+  },
+  {
+    "revision": "5290d1800941ffd1afe8841579a2a302",
+    "url": "/random_restaurants/static/js/2.79c83804.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8805cfad90a80658198",
+    "url": "/random_restaurants/static/js/main.94e07487.chunk.js"
+  },
+  {
+    "revision": "913221b28c29a6855a85",
+    "url": "/random_restaurants/static/js/runtime-main.a44f11b5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/random_restaurants/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/random_restaurants/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/random_restaurants/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/random_restaurants/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/random_restaurants/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
